aa5b19e1b9b8a22f449ff185db4205b355474146
Tim Hardy <tim.hardy@omegacreations.com>
2021-06-21 19:54:22 -0500

    added a helpful comment about serializable objects

--- src/app/products/product.model.ts
+++ src/app/products/product.model.ts
@@ -1,3 +1,5 @@
+// NOTE: if you create a typed model with a custom constructor you CANNOT pass it through ngRx state!
+//  you have to pass only raw objects through.  Easy fix (spread operator) - const rawObject = {...typedObject}
 export interface Product {
   id: number;
   name: string;
